6a9fc2102c8c900157cb9b8da7e4e8fb4f8bf39a
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2025-12-05 10:56:41 +0530

    in my side complete  website

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,58 +1,60 @@
-import nodemailer from "nodemailer";
-
-export async function POST(req) {
-  try {
-    const { name, phone, email, message } = await req.json();
-
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: "abhisheksahuacmeinfolab@gmail.com",
-      subject: `New Contact Form Message`,
-      text: `
-      Name: ${name}
-      Phone: ${phone}
-      Email: ${email}
-      Message:
-      ${message}
-       `,
-    });
-    return Response.json({ success: true });
-  } catch (error) {
-    console.log("Email error:", error);
-    return Response.json({ success: false, error });
-  }
-}
-
-// import { Resend } from "resend";
+// import nodemailer from "nodemailer";
 
 // export async function POST(req) {
 //   try {
-//     const { name, email, phone, message } = await req.json();
+//     const { name, phone, email, message } = await req.json();
 
-//     const resend = new Resend(process.env.RESEND_API_KEY);
-
-//     await resend.emails.send({
-//     //   from: "onboarding@resend.dev",
-//       from: "noreply@acmeinfolabs.com",
-//       to: "abhisheksahuacmeinfolab@gmail.com",
-//       subject: `New Message from ${name}`,
-//       phone: `Mobile No. ${phone}`,
-//       text: `Email: ${email}\n\n${message}`,
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.SMTP_HOST,
+//       port: Number(process.env.SMTP_PORT),
+//       secure: false,
+//       auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS,
+//       },
 //     });
 
+//     await transporter.sendMail({
+//       from: process.env.SMTP_USER,
+//       to: "abhisheksahuacmeinfolab@gmail.com",
+//       subject: `New Contact Form Message`,
+//       text: `
+//       Name: ${name}
+//       Phone: ${phone}
+//       Email: ${email}
+//       Message:
+//       ${message}
+//        `,
+//     });
 //     return Response.json({ success: true });
 //   } catch (error) {
-//     console.log(error);
-//     return Response.json({ success: false, error: error.message }, { status: 500 });
+//     console.log("Email error:", error);
+//     return Response.json({ success: false, error });
 //   }
 // }
+
+
+// Resend
+import { Resend } from "resend";
+
+export async function POST(req) {
+  try {
+    const { name, email, phone, message } = await req.json();
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    await resend.emails.send({
+    //   from: "onboarding@resend.dev",
+      from: "noreply@acmeinfolabs.com",
+      to: "abhisheksahuacmeinfolab@gmail.com",
+      subject: `New Message from ${name}`,
+      phone: `Mobile No. ${phone}`,
+      text: `Email: ${email}\n\n${message}`,
+    });
+
+    return Response.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ success: false, error: error.message }, { status: 500 });
+  }
+}
