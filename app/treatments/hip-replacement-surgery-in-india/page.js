2e4a1415f3d98dc0681a7a17536f29276b07553c
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2026-07-31 12:17:39 +0530

    Update page.js

--- app/treatments/hip-replacement-surgery-in-india/page.js
+++ app/treatments/hip-replacement-surgery-in-india/page.js
@@ -421,7 +421,7 @@ export default function HipReplacementIndia() {
       and choose the most appropriate treatment approach.
     </p>
      <p className="text-gray-700 mt-4">
-     If hip pain is related to a spinal disorder, expert<a href="https://www.ekamfertility.com/spine-surgery-in-india/" target="_blank" rel="noopener noreferrer"> <strong> spine surgery in India</strong> </a> may be recommended for effective treatment. 
+     If hip pain is related to a spinal disorder, expert<a href="https://www.ekamcure.com/treatments/best-spine-surgery-in-india" target="_blank" rel="noopener noreferrer"> <strong> spine surgery in India</strong> </a> may be recommended for effective treatment. 
     </p>
   </div>
 </section>
