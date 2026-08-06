0088fc4eca087dd8f8f563291bf7166f840236b9
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2026-08-06 15:46:15 +0530

    Update page.js
    Add link in "More Related Links"

--- app/doctors/best-prostate-cancer-doctors-in-india/page.js
+++ app/doctors/best-prostate-cancer-doctors-in-india/page.js
@@ -519,7 +519,16 @@ export default function BestProstateCancerDoctorsIndia() {
               </div>
           
               <div className="grid md:grid-cols-2 gap-5">
-          
+
+                <Link
+                  href="/treatments/best-prostate-cancer-treatment-india"
+                  className="group flex items-center justify-between bg-[#F8FAFD] border border-[#E1E8F0] rounded-xl px-5 py-4 hover:border-pink-500 hover:bg-gradient-to-r hover:from-[#053161] hover:to-[#6796cc] transition-all duration-300"
+                >
+                  <span className="font-medium text-[#053161] group-hover:text-white transition">
+                    → Best Prostate Cancer Treatment in India
+                  </span>
+                </Link>
+                    
                 <Link
                   href="/cost-of-treatment/prostate-cancer-treatment-cost-in-india"
                   className="group flex items-center justify-between bg-[#F8FAFD] border border-[#E1E8F0] rounded-xl px-5 py-4 hover:border-pink-500 hover:bg-gradient-to-r hover:from-[#053161] hover:to-[#6796cc] transition-all duration-300"
