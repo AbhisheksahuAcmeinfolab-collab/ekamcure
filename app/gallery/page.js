c6f23bc44e6da05bad3f2ef0c74bc8c61e919664
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2026-09-21 13:04:20 +0530

    Update page.js
    Changes

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -34,6 +34,7 @@ import g7 from "../../assets/gallery/g-7.jpeg";
 import g8 from "../../assets/gallery/g-8.jpg";
 import g9 from "../../assets/gallery/g-9.jpeg";
 import g11 from "../../assets/gallery/g-11.jpeg";
+import g12 from "../../assets/gallery/g-12.jpeg";
 
 const images = [
   { src: g1, title: "Marie Line (Mauritius)" },
@@ -42,10 +43,11 @@ const images = [
   { src: g4, title: "Fiji Patient" },
   { src: g5, title: "Eugenia Bepu" },
   { src: g6, title: "Mrs. Bhunu Letitia Tambudzai" },
-  { src: g7, title: "Amit Kumar (Fiji)" },
+  { src: g7, title: "Amit Kumar , Fiji" },
   { src: g8, title: "Mr. Vedanand Prayag" },
   { src: g9, title: "Patient Dubey" },
   { src: g11, title: "Medical Visa Support" },
+  { src: g12, title: "Treatment Success" },
   { src: pic23, title: "Vidya Wati" },
   { src: pic21, title: "Deepak Dhookoo" },
   { src: pic20, title: "Bissun Prasad" },
