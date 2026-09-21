c9768a6b5a2b0df91aaeead53edf0e691310d938
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2026-09-21 12:48:17 +0530

    Update page.js
    fix error

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -45,7 +45,6 @@ const images = [
   { src: g7, title: "Amit Kumar , Fiji" },
   { src: g8, title: "Mr. Vedanand Prayag" },
   { src: g9, title: "Patient Dubey" },
-  { src: g10, title: "Hospital Transfer" },
   { src: g11, title: "Medical Visa Support" },
   { src: g12, title: "Treatment Success" },
   { src: g13, title: "Ekam Health Care Team" },
