703d14297883b5ed5369a1ed99a7951250a4aca7
AbhisheksahuAcmeinfolab-collab <abhisheksahuacmeinfolab@gmail.com>
2026-09-26 12:52:32 +0530

    Update page.js
    Do correcct the link

--- app/cost-of-treatment/hip-replacement-surgery-cost-india-international-patients/page.js
+++ app/cost-of-treatment/hip-replacement-surgery-cost-india-international-patients/page.js
@@ -369,7 +369,7 @@ export default function HipReplacementCostPage() {
                   <p className="text-[#425466] text-base md:text-lg leading-8">
                     These expenses should be considered separately when calculating the{" "}
                     <Link
-                      href="/cost-of-treatment/hip-replacement-surgery-cost-india-international-patients"
+                      href="/blog/total-budget-medical-trip-to-india"
                       className="text-blue-600 hover:underline"
                     >
                       <strong>total budget for a medical trip to India</strong>
